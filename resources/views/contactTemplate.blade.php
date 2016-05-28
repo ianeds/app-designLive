c82a2a607c6f0225a5c636dee6c7b065a12c093b
Apparent Design <ian@apparentdesign.com>
2016-05-28 18:41:31 +0200

    added google tracking
    google tracking added to php templates, new php file tracking

--- resources/views/contactTemplate.blade.php
+++ resources/views/contactTemplate.blade.php
@@ -13,6 +13,7 @@
     @yield('head')
     </head>
 <body>
+<?php include_once("analyticstracking.php") ?>
     <header>
         <div class="header">
             <a href="/" ><img src="images/apparent-logo.png" width="231" height="70"></a>
