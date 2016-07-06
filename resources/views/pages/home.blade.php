1cdbfaf766b67339cba21a4dd523f4aef12ca751
Apparent Design <ian@apparentdesign.com>
2016-07-06 17:47:12 +0200

    changed titles on home page

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -4,16 +4,16 @@
 <main id="mainColumn">
 	<div class="container">
 
-		<a href="/projects"><div class="previewItem" id="pre_sml"><img class="" src="images/triptych_1_700x540.png"><span>Illustration</span></div></a>
+		<a href="/projects"><div class="previewItem" id="pre_sml"><span>Illustration</span><img class="" src="images/triptych_1_700x540.png"></div></a>
 
 
-        <a href="/touchscreen"><div class="first previewItem"><img class="" src="images/touchscreens_pre.png"><span>Interface design</span></div></a>
+        <a href="/touchscreen"><div class="first previewItem"><span>Interface design</span><img class="" src="images/touchscreens_pre.png"></div></a>
 
-		<a href="/inter"><div class="previewItem" id="pre_sml"><img class="" src="images/xbox_pre.png"><span>Web design</span></div></a>
+		<a href="/inter"><div class="previewItem" id="pre_sml"><span>Web design</span><img class="" src="images/xbox_pre.png"></div></a>
 
-		<a href="/type"><div class="previewItem"><img class="" src="images/trop_pre.png"><span>Typography</span></div></a>
+		<a href="/type"><div class="previewItem"><span>Typography</span><img class="" src="images/trop_pre.png"></div></a>
          
-        <a href="/bannerAds"><div class="previewItem"><img class="" src="images/bannerAds_pre.png"><span>Banner ads</span></div></a>
+        <a href="/bannerAds"><div class="previewItem"><span>Banner ads</span><img class="" src="images/bannerAds_pre.png"></div></a>
 
     </div> <!-- end .container -->
 
