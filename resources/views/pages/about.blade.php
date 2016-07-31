d5af1b49e00fefd7dc6b56b87992747f7f6d9bc6
Apparent Design <ian@apparentdesign.com>
2016-07-31 15:30:40 +0200

    update web design section

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -6,9 +6,9 @@
     <div class="contactItem primary">
         <p>Lamp Bureau is a graphic design agency that helps businesses, organizations, and people communicate their brand message through expressive and disciplined visual design.<br><br> Multidisciplinary and concept-driven, Lamp works across digital and print media. Our three main areas of service are:
         <ul class="servicesList">
-            <li>Web design: websites and other interactive applications
+            <li>Web design: interface design, front-end web development using CSS 3, HTML5, Javascript/jQuery, PHP, and Laravel 5.
             </li>
-            <li>Print design: posters, catalogues, publications and other associated collateral</li>
+            <li>Print design: posters, publications, catalogues and other associated collateral</li>
             <li>Identity and branding: logos and logo systems, style guides, brand concepting</li>
         </ul> 
 
