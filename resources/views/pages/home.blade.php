750603e1990ee7357677b3e235a0131d9853a27c
Apparent Design <ian@apparentdesign.com>
2016-08-01 10:56:31 +0200

    changes

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -5,23 +5,23 @@
 	<div class="container">
 
 
-        <div class="previewItem"><div class="qualitytime"><a href="/qualitytime"><img src="/images/quality-time_700x540.png"></a></div><span class="preview-mobile-text top-padding"><span class="ital">Quality time</span> illustration</span></div>
+        <div class="previewItem"><div class="qualitytime"><a href="/qualitytime"><img src="/images/quality-time_700x540.png"></div><span class="preview-mobile-text top-padding"><span class="ital">Quality time</span> illustration</span></a></div>
         
-		<div class="previewItem"><div class="wellthen"><a href="/wellthen"><img src="images/well-then-2_700x540.png"></a></div><span class="preview-mobile-text"><span class="ital">Well then</span> illustration</span></div>
+		<div class="previewItem"><div class="wellthen"><a href="/wellthen"><img src="images/well-then-2_700x540.png"></div><span class="preview-mobile-text"><span class="ital">Well then</span> illustration</span></a></div>
 
-        <div class="previewItem"><div class="touchscreen"><a href="/touchscreen"><img src="images/touchscreens_pre.png"></a></div><span class="preview-mobile-text top-padding">Touch-screen kiosk designs</span></div>
+        <div class="previewItem"><div class="touchscreen"><a href="/touchscreen"><img src="images/touchscreens_pre.png"></div><span class="preview-mobile-text top-padding">Touch-screen kiosk designs</span></a></div>
 
-        <div class="previewItem"><div class="diamonds"><a href="/diamonds"><img src="images/diamond_1_700x540.png"></a></div><span class="preview-mobile-text top-padding"><span class="ital">Diamonds</span> illustrations</span></div>
+        <div class="previewItem"><div class="diamonds"><a href="/diamonds"><img src="images/diamond_1_700x540.png"></div><span class="preview-mobile-text top-padding"><span class="ital">Diamonds</span> illustrations</span></a></div>
 
-        <div class="previewItem"><div class="inter"><a href="/inter"><img src="images/inter-700x540.png"></a></div><span class="preview-mobile-text top-padding">Web design</span></div>
+        <div class="previewItem"><div class="inter"><a href="/inter"><img src="images/inter-700x540.png"></div><span class="preview-mobile-text top-padding">Web design</span></a></div>
 
-        <div class="previewItem"><div class="simplethings"><a href="/simplethings"><img src="images/simple_things_700x540.png"></a></div><span class="preview-mobile-text"><span class="ital">Simple things</span> illustration</span></div>
+        <div class="previewItem"><div class="simplethings"><a href="/simplethings"><img src="images/simple_things_700x540.png"></div><span class="preview-mobile-text"><span class="ital">Simple things</span> illustration</span></a></div>
 
-        <div class="previewItem"><div class="jusquabout"><a href="/jusquabout"><img src="images/jusqu-au-bout-700x540.png"></a></div><span class="preview-mobile-text"><span class="ital">Jusqu'au bout</span> illustration</span></div>
+        <div class="previewItem"><div class="jusquabout"><a href="/jusquabout"><img src="images/jusqu-au-bout-700x540.png"></div><span class="preview-mobile-text"><span class="ital">Jusqu'au bout</span> illustration</span></a></div>
 
-        <div class="previewItem"><div class="sqpat"><a href="/sqpat"><img src="images/squarePattern1_700x540.png"></a></div><span class="preview-mobile-text">Square pattern illustrations</span></div>
+        <div class="previewItem"><div class="sqpat"><a href="/sqpat"><img src="images/squarePattern1_700x540.png"></div><span class="preview-mobile-text">Square pattern illustrations</span></a></div>
 
-        <div class="previewItem"><div class="trop"><a href="/trop"><img src="images/trop_700x540.png"></a></div><span class="preview-mobile-text"><span class="ital">Trop</span> illustration</span></div>
+        <div class="previewItem"><div class="trop"><a href="/trop"><img src="images/trop_700x540.png"></div><span class="preview-mobile-text"><span class="ital">Trop</span> illustration</span></a></div>
 
     </div> <!-- end .container -->
 
